e3eb9adcc571d237e0125b3a54c487ecf5db9b63
Teguh Pratama <teguhpratama337@gmail.com>
2023-06-18 16:07:44 +0900

    try-middleware

--- middleware.ts
+++ middleware.ts
@@ -23,7 +23,7 @@ export async function middleware(req: NextRequest) {
       return NextResponse.redirect(new URL('/', req.url));
     }
 
-    if (!verifiedToken && !req.nextUrl.pathname.startsWith('/login')) {
+    if (!verifiedToken && !req.nextUrl.pathname.startsWith('/')) {
       return NextResponse.redirect(new URL('/login', req.url));
     }
 
